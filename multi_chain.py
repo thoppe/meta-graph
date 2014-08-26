9a762b9f06a711e682c633922a6af722acb9e13b
thoppe <travis.hoppe@gmail.com>
2014-08-26 17:04:33 -0400

    Fixed multichain

--- multi_chain.py
+++ multi_chain.py
@@ -89,20 +89,6 @@ class multi_Manager(object):
         self._empty_source = False
         self._is_complete  = False
 
-        self.shutdown_level = 0
-        self.shutdown_intra_level = 0
-
-        # This is the order things get shutdown
-        # the inital input is always mapped to the source
-        #
-        #self.close_mapping = {self.T_input[0] : (lambda x: self._empty_source)}
-        #for k,TX in enumerate(self.T_input):
-        #    if k>0:
-        #        self.close_mapping[TX] = is_consumer_level_alive(task_n)
-        #print self.close_mapping
-        #exit()
-        
-
     def consumer_iter(self):
         for C_block in self.C:
             for c in C_block: yield c
@@ -122,18 +108,11 @@ class multi_Manager(object):
         S_out = self.S_output[k]
         f = self.func_S[k]
 
-        print "PROCESS SERIAL", k, S_in.qsize(), consumers[0].is_alive()
-
         while S_in.qsize():
             val    = S_in.get()
             if not type(val)==PoisonPill:
                 result = f(val)
                 S_out.put(result)
-            else:
-                S_out.put(val)
-        #else:
-        #    print "HERE!"
-        #    exit()
 
     def process_final_serial(self):
         while not self.S_output[-1].empty():
@@ -165,7 +144,6 @@ class multi_Manager(object):
     def load_queue(self, k, free_tasks=10**8):
         # Step through queues backwards
         # ignore first T and last S
-        print "SDJFJ"
 
         consumers = self.C[k]
         S_out = self.S_output[k]
@@ -177,8 +155,7 @@ class multi_Manager(object):
             if type(val) != PoisonPill:
                 task = Generic_Task(f, val)
                 T_in.put(task)
-            #else:
-            #    T_in.put(val)
+
             free_tasks -= 1
             if not free_tasks: return False
 
@@ -187,61 +164,29 @@ class multi_Manager(object):
             
     def shutdown(self):
 
-        level = self.shutdown_level
-        intra_level = self.shutdown_intra_level
+        logger.debug("Shutting down")
+        logger.debug("Check status: %s" % self.get_qsize()) 
 
-        print "SHUTDOWN *****************", level, intra_level, self.task_n
-
-
-        if level == self.task_n-1 and intra_level==1:
-            print "SHUTDOWN COMPLETE *************"
-
-            Q1  = self.all_Q[1][level]
-            self._poison_queue(Q1)
-
-            Q2  = self.all_Q[2][level]
-            self._poison_queue(Q2)
-            
-            #self.process_serial(level)
-            #self._poison_queue(Q)
-            print "CHECK:", self.S_output[level].qsize()
-            exit()
-            self.is_complete = True
-            return True
-
-        if intra_level == 0:
-            logger.debug("Check status: %s" % self.get_qsize()) 
-            print "Shuting down level", level, intra_level
-
+        for level in range(self.task_n):          
             Q  = self.all_Q[0][level]
             self._poison_queue(Q)
             self.process_serial(level)
 
-            #Q.close()
-            #Q.join_thread()
-
-            self.shutdown_intra_level += 1
-            
-            logger.debug("Check status: %s" % self.get_qsize())        
-
-        if intra_level == 1:
-            logger.debug("Check status: %s" % self.get_qsize())        
-            print "Shuting down level", level, intra_level
-
-            Q1  = self.all_Q[1][level]
-            self._poison_queue(Q1)
+            Q  = self.all_Q[1][level]
+            self._poison_queue(Q)
             self.process_serial(level)
 
-            Q2  = self.all_Q[2][level]
-            self._poison_queue(Q2)
-            self.load_queue(level)
+            Q  = self.all_Q[2][level]
+            self._poison_queue(Q)
 
-            print "CHECK:", self.S_output[level].qsize()
+            if level < self.task_n-1:
+                self.load_queue(level)
+            else:
+                self.process_final_serial()
 
-            self.shutdown_intra_level = 0
-            self.shutdown_level += 1
+            logger.debug("Check status: %s" % self.get_qsize()) 
 
-        return False
+        self._is_complete  = True
 
     
     def is_empty(self):
@@ -257,18 +202,19 @@ class multi_Manager(object):
 
     def cycle(self):
 
-        for k in range(self.shutdown_level, self.task_n):
+        for k in range(self.task_n):
             self.process_serial(k)
 
         # Pulling anything off of the last serial queue and discard it
         self.process_final_serial()
 
         free = self.count_free_slots()
+
         if not free: return False
 
         # Step through queues backwards
         # ignore first T and last S
-        for k in range(self.shutdown_level, self.task_n-1):
+        for k in range(self.task_n-1):
             status = self.load_queue(k, free)
             if not status: return status
 
@@ -289,12 +235,11 @@ class multi_Manager(object):
 # These test methods have to be global to work with 'spawn' method!
 
 def serial_a(x):
-    #print ("A result: ", x)
+    print ("A result: ", x)
     return x
 
 def serial_b(x):
-    #print ("B result: ", x)
-    time.sleep(.1)
+    print ("B result: ", x)
     return x
 
 def mul2(x):
@@ -307,11 +252,11 @@ if __name__ == "__main__":
 
     #multiprocessing.set_start_method('spawn')
 
-    source = iter(range(5))
+    source = iter(range(2**6))
 
     M = multi_Manager(source, [mul2,sub3], [serial_a, serial_b], 
                       chunksize=10,
-                      procs=1)
+                      procs=8)
     M.run()
 
     print ("Finished gracefully!")
