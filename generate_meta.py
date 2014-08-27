8f6809637e981e0cdd4987bb5f93f361d3f4efa0
thoppe <travis.hoppe@gmail.com>
2014-08-26 23:02:33 -0400

    linted generate_meta

--- generate_meta.py
+++ generate_meta.py
@@ -1,6 +1,8 @@
-import sqlite3, logging, argparse, os, collections
-import subprocess, itertools
-import numpy as np
+import sqlite3
+import logging
+import argparse
+import collections
+import itertools
 
 import graph_tool
 import graph_tool.topology
@@ -11,28 +13,34 @@ from src.helper_functions import grab_all, select_itr
 from src.invariants import graph_tool_representation
 import src.invariants as invar
 
-desc   = "Creates the connected simple edge meta-graph of order N"
+desc = "Creates the connected simple edge meta-graph of order N"
 parser = argparse.ArgumentParser(description=desc)
-parser.add_argument('N',type=int,default=4,
+parser.add_argument('N', type=int, default=4,
                     help="graph order")
 parser.add_argument('--draw', default=False, action='store_true')
 parser.add_argument('--clear', default=False, action='store_true',
                     help="Clears this value from the database")
-parser.add_argument('--procs', default=None, type=int,
-                    help="Number of multiprocessers to use for each multitask")
 parser.add_argument('--force', default=False, action='store_true',
                     help="Clears and starts computation")
+parser.add_argument('--procs', default=None, type=int,
+                    help="Number of multiprocessers to use for each multitask")
+parser.add_argument('--chunksize', default=50, type=int,
+                    help="Chunks to feed to the multiprocessers")
+
 
 def is_connected(g):
+    ''' Returns 1 if a graph-tool graph is connected '''
     component_size = graph_tool.topology.label_components(g)[1]
-    return len(component_size)==1
+    return len(component_size) == 1
+
 
 def iso_difference_in_set(g, iso_set):
     ''' Find an isomorphism in a list and return it, otherwise return True'''
     for h in iso_set:
-        if graph_tool.topology.isomorphism(g,h):
-            return False,h
-    return True,None
+        if graph_tool.topology.isomorphism(g, h):
+            return False, h
+    return True, None
+
 
 def connected_cut_iterator(g):
     ''' Iterates over all graphs with one less edge that are still connected '''
@@ -44,102 +52,123 @@ def connected_cut_iterator(g):
         # Check if connected
         if is_connected(gx):
             yield gx
-    
+
 
 def find_iso_set_from_cut(g):
-    # For every edge in the current graph,
-    # find isomorphicly distinct set of edge removals and their cardinality
+    ''' For every edge in the current graph,
+        find isomorphicly distinct set of edge removals
+        and their cardinality '''
 
     iso_set = collections.Counter()
 
     for h in connected_cut_iterator(g):
-       
+
         is_unique, h_iso = iso_difference_in_set(h, iso_set)
-        if is_unique: 
+        if is_unique:
             iso_set[h] += 1
-        else: 
+        else:
             iso_set[h_iso] += 1
 
     return iso_set
 
-def possible_laplacian_match(L):
-    return [ (k,ADJ[k]) for k in  LPOLY[L]]
 
 def identify_match_from_adj(g, match_set):
-    # Sanity check here, remove comments for speed
-    #if len(match_set) == 1:
-    #    return match_set[0][0]
+    '''
+    Given a graph g, and a set of isomorphically distinct graphs
+    in match_set (saved as list of two tuples, idx+adj), find
+    the match in the set. Assume that the match is possible and check one less.
+    '''
 
-    for idx,adj in match_set:
-        h = graph_tool_representation(adj,N=N)
-        if graph_tool.topology.isomorphism(g,h):
+    for idx, adj in match_set[:-1]:
+        h = graph_tool_representation(adj, N=N)
+        if graph_tool.topology.isomorphism(g, h):
             return idx
 
-    raise ValueError("should find match already")
+    return match_set[-1][0]
+    # raise ValueError("should find match already")
 
-def compute_valid_cuts( item ):
+
+def compute_valid_cuts(item):
+    '''
+    Build a graph from adj and determine the unique graphs that can
+    be made by removing a single edge. For each isomorphically distinct
+    graph, compute the Laplacian. '''
+
     e0, target_adj = item
 
     # Determine the valid iso_set and compute the invariant (Laplacian)
-    g = graph_tool_representation(target_adj,N=N)
+    g = graph_tool_representation(target_adj, N=N)
     iso_set = find_iso_set_from_cut(g)
 
-    #laplacian_set = collections.Counter()
     laplacian_map = dict()
 
-    HL = []
     for h in iso_set:
         A = graph_tool.spectral.adjacency(h)
-        laplacian_map[h] = invar.special_laplacian_polynomial(A,N=N)
+        laplacian_map[h] = invar.special_laplacian_polynomial(A, N=N)
 
     return e0, g, iso_set, laplacian_map
 
+
+def possible_laplacian_match(L):
+    ''' Determine which graphs have this particular Laplacian polynomial '''
+
+    return [(k, ADJ[k]) for k in LPOLY[L]]
+
+
 def process_lap_poly(item):
-    e0, g, iso_set,L_MAP = item
+    ''' Serial function to determine which the graphs
+    in the Laplacian database '''
+    e0, g, iso_set, L_MAP = item
 
     L_GRAPH_MAP = {}
-    for h,L in L_MAP.items():
+    for h, L in L_MAP.items():
         match_set = possible_laplacian_match(L)
         L_GRAPH_MAP[h] = match_set
     return (e0, g, iso_set, L_GRAPH_MAP)
 
+
 def process_match_set(item):
-    e0,g,iso_set,L_GRAPH_MAP = item
+    ''' Determine the weights of the meta edges. '''
+
+    e0, g, iso_set, L_GRAPH_MAP = item
 
     E1_weights = {}
     E0_weights = collections.defaultdict(int)
-    for h,match_set in L_GRAPH_MAP.items():
-        e1 = identify_match_from_adj(h,match_set)
-        E1_weights[e1] = iso_set[h] 
+    for h, match_set in L_GRAPH_MAP.items():
+        # Figure out the forward weights
+        e1 = identify_match_from_adj(h, match_set)
+        E1_weights[e1] = iso_set[h]
 
         # Now determine the reverse weight
-        for v1,v2 in itertools.combinations(h.vertices(),2):
+        for v1, v2 in itertools.combinations(h.vertices(), 2):
             h2 = h.copy()
-            h2.add_edge(v1,v2)
-            E0_weights[e1] += graph_tool.topology.isomorphism(g,h2)
+            h2.add_edge(v1, v2)
+            E0_weights[e1] += graph_tool.topology.isomorphism(g, h2)
 
-    return (e0,E1_weights,E0_weights)
+    return (e0, E1_weights, E0_weights)
+
 
 def record_E1_set(item):
+    ''' Save the weights into the meta_conn database. '''
 
-    e0,E1_weights,E0_weights = item
+    e0, E1_weights, E0_weights = item
 
     cmd_insert = '''
-    INSERT INTO metagraph 
+    INSERT INTO metagraph
     (meta_n,e0,e1,weight,direction)
     VALUES (?,?,?,?,?)
     '''
 
     def edge_insert_itr():
         for e1 in E1_weights:
-            yield (N,e0,e1,E1_weights[e1],0)
+            yield (N, e0, e1, E1_weights[e1], 0)
         for e1 in E0_weights:
-            yield (N,e1,e0,E0_weights[e1],1)
+            yield (N, e1, e0, E0_weights[e1], 1)
 
     logging.info("Computed e0 ({})".format(e0))
     meta_conn.executemany(cmd_insert, edge_insert_itr())
 
-#######################################################################
+#
 
 cargs = vars(parser.parse_args())
 N = cargs["N"]
@@ -148,18 +177,19 @@ N = cargs["N"]
 logging.root.setLevel(logging.INFO)
 
 # Connect to the database
-conn  = load_graph_database(N)
-sconn = load_graph_database(N,special=True)
+conn = load_graph_database(N)
+sconn = load_graph_database(N, special=True)
 
 # Build the source generator, tuple of (graph_id, adj)
 cmd_grab = '''SELECT graph_id,adj FROM graph'''
-source = select_itr(conn,cmd_grab)
+source = select_itr(conn, cmd_grab)
 
 # Build the multichain process early while nothing is allocated
 from multi_chain import multi_Manager
-MULTI_TASKS  = [compute_valid_cuts,process_match_set]
-SERIAL_TASKS = [process_lap_poly,record_E1_set]
-M = multi_Manager(source, MULTI_TASKS, SERIAL_TASKS,chunksize=50,
+MULTI_TASKS = [compute_valid_cuts, process_match_set]
+SERIAL_TASKS = [process_lap_poly, record_E1_set]
+M = multi_Manager(source, MULTI_TASKS, SERIAL_TASKS,
+                  chunksize=cargs["chunksize"],
                   procs=cargs["procs"])
 
 # Connect to the meta database and template it if needed
@@ -187,12 +217,12 @@ if cargs["clear"] or cargs["force"]:
 
 # Check if values have been computed, if so, exit early
 cmd_check_complete = '''SELECT meta_n FROM computed'''
-complete_n = grab_vector(meta_conn,cmd_check_complete)
+complete_n = grab_vector(meta_conn, cmd_check_complete)
 if N in complete_n:
     msg = "meta {} has already been computed, exiting".format(N)
     M.shutdown()
     raise ValueError(msg)
-    
+
 # Make a mapping of all the graph id's
 logging.info("Loading the graph adj information")
 ADJ = dict(grab_all(conn, '''SELECT graph_id,adj FROM graph'''))
@@ -205,11 +235,11 @@ WHERE graph_id = (?) ORDER BY x_degree,coeff'''
 LPOLY = collections.defaultdict(list)
 
 for gid in ADJ:
-    L = tuple(grab_all(sconn, single_grab, (gid,)))   
+    L = tuple(grab_all(sconn, single_grab, (gid,)))
     LPOLY[L].append(gid)
 
 num_LPOLY = len(LPOLY)
-num_ADJ   = len(ADJ)
+num_ADJ = len(ADJ)
 if not num_LPOLY:
     msg = "LPOLY database is empty"
     raise ValueError(msg)
@@ -223,37 +253,28 @@ logging.info("Starting edge remove computation")
 M.run()
 
 cmd_mark_complete = "INSERT INTO computed VALUES (?)"
-meta_conn.execute(cmd_mark_complete,(N,))
+meta_conn.execute(cmd_mark_complete, (N,))
 meta_conn.commit()
 
-# Sanity check on the number of rows
-#cmd_rows = "SELECT e0 FROM metagraph WHERE meta_n={}".format(N)
-#E0_rows = set( grab_vector(meta_conn,cmd_rows) )
-#print "Missing rows: ", set(ADJ.keys()).difference(E0_rows)
-#exit()
-
 cmd_select = '''
-SELECT e0,e1 FROM metagraph 
+SELECT e0,e1 FROM metagraph
 WHERE meta_n={} AND direction=0'''.format(N)
 
 m = graph_tool.Graph(directed=False)
 m.add_vertex(len(ADJ))
 
-# Draw the undirected case
-for (i,j) in select_itr(meta_conn, cmd_select):
-    m.add_edge(i-1,j-1)
+# Draw the undirected case, direction==0
+for (i, j) in select_itr(meta_conn, cmd_select):
+    m.add_edge(i - 1, j - 1)
 
 f_png = "figures/meta_simple_{}.png".format(N)
-logging.info("Saving %s"%f_png)
+logging.info("Saving %s" % f_png)
 
-print ("Saving")
+print("Saving")
 f_save = "reps/meta_{}.gml".format(N)
 m.save(f_save)
 
-print ("Drawing")
-graph_tool.draw.graphviz_draw(m,layout="dot",
+print("Drawing")
+graph_tool.draw.graphviz_draw(m, layout="dot",
                               output=f_png,
-                              vsize=.4,penwidth=4,size=(30,30))
-
-#import pylab as plt
-#plt.show()
+                              vsize=.4, penwidth=4, size=(30, 30))
